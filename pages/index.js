42e66bfecf6445e0969f12011fdde0e896192e9e
Hallison Brancalhão <98134774+hallisonbrancalhao@users.noreply.github.com>
2023-08-26 19:49:57 +0000

    criando uma pagina de construção

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,29 @@
 function Home() {
-  return <h1>A paciencia é uma virtude.</h1>;
+  return (
+    <>
+      <h1>Clone-TabNews: Documentação do Projeto</h1>
+      <p>
+        <strong>Status:</strong> Em construção
+      </p>
+
+      <h2>Índice</h2>
+      <ul>
+        <li>
+          <a href="#introducao">Introdução</a>
+        </li>
+        <li>
+          <a href="#pre-requisitos">Pré-requisitos</a>
+        </li>
+      </ul>
+
+      <h2 id="pre-requisitos">Pré-requisitos</h2>
+      <ul>
+        <li>Node.js</li>
+        <li>NPM ou Yarn</li>
+        <li>Git</li>
+      </ul>
+    </>
+  );
 }
 
 export default Home;
