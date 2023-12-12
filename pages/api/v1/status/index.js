a2328d21ac61f30b79c62181e5635df35cd05d92
Hallison Brancalhão <98134774+hallisonbrancalhao@users.noreply.github.com>
2023-12-12 17:23:35 +0000

    retornando  e  da api

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,24 @@
 import database from 'infra/database.js'
 
 async function status(request, response) {
-  const result = await database.query(`SELECT 1 + 1 as SUM;`)
-  console.log(result.rows);
-    response.status(200).json({chave: "valor"})
+  const updatedAt = new Date().toISOString()
+  const dabaseVersionResult = await database.query("SHOW server_version;")
+  const databaseVersionValue = dabaseVersionResult.rows[0].server_version
+
+  const databaseMaxConenectionsResult = await database.query("SHOW max_connections;")
+  const databaseMaxConenectionsValue = databaseMaxConenectionsResult.rows[0].max_connections;
+
+  response.status(200).json(
+    {
+      "updated_at": updatedAt,
+      "dependencies": {
+        "database": {
+          "version": databaseVersionValue,
+          "max_connections": parseInt(databaseMaxConenectionsValue)
+        }
+      }
+    }
+  )
 }
 
 export default status
