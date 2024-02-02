31b918f38dfe8291edf7a41f822d3e5285a573b5
Hallison Brancalhão <98134774+hallisonbrancalhao@users.noreply.github.com>
2024-02-02 13:09:11 +0000

    makes   more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -9,18 +9,18 @@ async function query(queryObject){
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   })
-  await client.connect()
 
-  try{
-    return await client.query(queryObject)
-  } catch(error) {
-    console.error(error);
+  try {
+    await client.connect()
+    const result = await client.query(queryObject)
+    return result 
+  } catch (error) {
+    console.error(error)
+    throw error;
   } finally {
     await client.end()
   }
 
-
-  return result 
 }
 
 export default {
