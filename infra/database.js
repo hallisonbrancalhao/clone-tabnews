463ab5a6866026b3f861c4430a53b6a7836b558c
Hallison Brancalhão <98134774+hallisonbrancalhao@users.noreply.github.com>
2024-02-09 13:14:21 +0000

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -8,6 +8,7 @@ async function query(queryObject){
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === 'development' ? false : true,
   })
 
   try {
@@ -18,7 +19,7 @@ async function query(queryObject){
     console.error(error)
     throw error;
   } finally {
-    await client.end()
+     await client.end()
   }
 
 }
