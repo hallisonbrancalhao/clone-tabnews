c730cf564d58e964518a6e786682af8891febe7e
Hallison Brancalhão <98134774+hallisonbrancalhao@users.noreply.github.com>
2025-02-10 16:48:58 +0000

    build: install `swr`

---
+++ pages/status/index.js
@@ -0,0 +1,58 @@
+import useSWR from "swr";
+
+async function fetchAPI(key) {
+  const response = await fetch(key);
+  const responseBody = await response.json();
+  return responseBody;
+}
+
+export default function StatusPage() {
+  return (
+    <>
+      <h1>Status</h1>
+      <UpdatedAt />
+      <hr />
+      <Database />
+    </>
+  )
+}
+
+function UpdatedAt() {
+  const { data, isLoading } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+
+  let updatedAtText = "Carregando...";
+  
+  if (!isLoading && data) {
+    updatedAtText = new Date(data.updated_at).toLocaleString();
+  }
+
+  return <div>Última atualização: {updatedAtText}</div>
+}
+
+function Database() {
+  const { data, isLoading } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+
+  let versionText = "Carregando...";
+  let maxConnectionsText = "Carregando...";
+  let usedConnectionsText = "Carregando...";
+
+  if (!isLoading && data) {
+    versionText = data.dependencies.database.version;
+    maxConnectionsText = data.dependencies.database.max_connections;
+    usedConnectionsText = data.dependencies.database.opened_connection;
+  }
+  return (
+    <div>
+      <h3>Banco de dados:</h3>
+      <ul>
+        <li>Versão: {versionText}</li>
+        <li>Conexões máximas: {maxConnectionsText}</li>
+        <li>Conexões abertas: {usedConnectionsText}</li>
+      </ul>
+    </div>
+  );
+}
